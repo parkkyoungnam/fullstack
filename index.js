87d689fa571b3b954ebcbca420540e7a0648a2fe
parkkyoungnam <pkn0813@naver.com>
2022-10-29 20:03:34 +0900

    fullstacktest

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-1
+const express = require('express')
+const app = express()
+const port = 3000
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://pkn0813:qkrrudska1!@cluster0.9bqodje.mongodb.net/?retryWrites=true&w=majority', {
+
+}).then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err));
+
+
+
+app.get('/', (req, res) => res.send('hello world!'))
+
+app.listen(port, () => console.log(`example app listening on port ${port}`))
